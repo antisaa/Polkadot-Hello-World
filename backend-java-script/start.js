db984c8f22de0f2b059082cff171751fbaa76449
Ante Sosa <antesosa95@gmail.com>
2020-10-18 02:12:56 +0200

    Add screenshoot

--- backend-java-script/start.js
+++ backend-java-script/start.js
@@ -1,7 +1,18 @@
 const { ApiPromise, WsProvider } = require("@polkadot/api");
 
+function printBlock(blockHash, blockHeader){
+  console.log(`------------------------\nDetails about block\n------------------------`);
+  console.log(`Block hash\t ${blockHash}`);
+  console.log(`Block number\t ${blockHeader.number.toString()}`);
+  console.log(`Author\t\t ${blockHeader.author.toString()}`);
+  console.log(`Parent hash\t ${blockHeader.parentHash.toHex()}`);
+  console.log(`State root\t ${blockHeader.stateRoot.toHex()}`);
+  console.log(`Eternistic root\t ${blockHeader.extrinsicsRoot.toHex()}`);
+}
 
 async function main() {
+  const params = process.argv.slice(2);
+
   const provider = new WsProvider("wss://kusama-rpc.polkadot.io");
   const api = await ApiPromise.create({ provider });
 
@@ -14,15 +25,16 @@ async function main() {
     `You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`
   );
 
+  if (params[0] == '--hash' && params[1]) {
+    const block = await api.rpc.chain.getBlock(params[1]);
+    const blockHeader = await api.derive.chain.getHeader(params[1]);
+    printBlock(params[1], blockHeader)
+  }
+  else {
     const blockHash = await api.rpc.chain.getBlockHash();
     const blockHeader = await api.derive.chain.getHeader(blockHash);
-    console.log(`------------------------\nDetails about last block\n------------------------`);
-    console.log(`Block hash\t ${blockHash}`);
-    console.log(`Block number\t ${blockHeader.number.toString()}`);
-    console.log(`Author\t\t ${blockHeader.author.toString()}`);
-    console.log(`Parent hash\t ${blockHeader.parentHash.toHex()}`);
-    console.log(`State root\t ${blockHeader.stateRoot.toHex()}`);
-    console.log(`Eternistic root\t ${blockHeader.extrinsicsRoot.toHex()}`);
+    printBlock(blockHash, blockHeader)
+  }
 }
 
 
